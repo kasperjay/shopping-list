da26030ce5b2818ff559805fc44f5ebd1ce7d7b1
Kasper Pickett <kasperpickett@gmail.com>
2019-10-27 22:55:09 -0500

    delete + check button functionality

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 $(function(){
-    $('#js-shopping-list-form').submit(function(event) {
-      event.preventDefault();
-      const listItem = $('#shopping-list-entry').val();
+  $('#js-shopping-list-form').submit(function(event) {
+    //prevents form submission default behavior
+    event.preventDefault();
+    const listItem = $('#shopping-list-entry').val();
 
-      $('#shopping-list-entry').val('');
+    //clears input field upon submission
+    $('#shopping-list-entry').val('');
 
-
-      $('.shopping-list').append(
-        `<li>
-          <span class="shopping-item">${listItem}</span>
-          <div class="shopping-item-controls">
-            <button class="shopping-item-toggle">
-              <span class="button-label">check</span>
-            </button>
-            <button class="shopping-item-delete">
-              <span class="button-label">delete</span>
-            </button>
-          </div>
-        </li>`);
+    //creates new list entry using the input text, includes check and delete buttons
+    $('.shopping-list').append(
+      `<li>
+        <span class="shopping-item">${listItem}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`);
     });
 
-  });
+    //clicking 'delete' button removes list entry
+    $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+      $(this).closest('li').remove();
+    });
+
+    //clicking 'check' button toggles strikethrough on item text
+    $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+      $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+    });
+});
